7bb0f448d1b816b59fa0810e86679342c5244a5e
nenziluca03-jpg <nenzi.luca03@gmail.com>
2026-07-20 17:24:01 +0200

    Add files via upload

---
+++ server.py
@@ -0,0 +1,6 @@
+MONGO_URL="mongodb://localhost:27017"
+DB_NAME="quantedge"
+CORS_ORIGINS="*"
+
+# Chiedi la tua chiave gratuita su https://emergent.sh (Profile → Universal Key)
+EMERGENT_LLM_KEY=sk-emergent-XXXXXXXXXXXXXX
